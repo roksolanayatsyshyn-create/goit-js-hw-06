7545bf2c48de19ca1efc45fe5a7eb5f281bf3bc9
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2025-11-04 15:52:09 +0200

    Comment out removeItem method implementation
    Comment out the removeItem method and update references to items.

--- js/task-2.js
+++ js/task-2.js
@@ -8,10 +8,10 @@ class Storage{
   addItem(newItem){
     this.items.push(newItem)
   }
-  // removeItem(itemToRemove){
-  //   const indexRemove=this.items.indexOf(itemToRemove)
+   // removeItem(itemToRemove){
+  //   const indexRemove=this.#items.indexOf(itemToRemove)
   //   if(indexRemove!==-1){
-  //   this.items.splice(indexRemove,1)
+  //   this.#items.splice(indexRemove,1)
   // }}
   removeItem(itemToRemove){
     this.items=this.items.filter(item=> item!==itemToRemove)
