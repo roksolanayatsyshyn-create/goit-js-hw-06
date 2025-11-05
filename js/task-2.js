fef77ed94dd26104930dcb2cd7bd6e409c67e6e8
Roksolana Fedyna <roksolanayatsyshyn@gmail.com>
2025-11-05 10:37:35 +0200

    private

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,21 @@
 class Storage{
+  #items;
   constructor(items){
-    this.items=items
+    this.#items=items
   }
   getItems(){
-    return this.items
+    return this.#items
   }
   addItem(newItem){
-    this.items.push(newItem)
+    this.#items.push(newItem)
   }
-   // removeItem(itemToRemove){
+  //  removeItem(itemToRemove){
   //   const indexRemove=this.#items.indexOf(itemToRemove)
   //   if(indexRemove!==-1){
   //   this.#items.splice(indexRemove,1)
   // }}
   removeItem(itemToRemove){
-    this.items=this.items.filter(item=> item!==itemToRemove)
+    this.#items=this.#items.filter(item=> item!==itemToRemove)
   }
 }
 
